eee555c8d0edf4cd05bc0d36c45868b0501cb313
riga <r.gmencia@gmail.com>
2022-10-22 07:36:36 +0100

    Updated ticker_converter.py to change PSTI and remove ZY.

--- src/modules/ticker_converter.py
+++ src/modules/ticker_converter.py
@@ -34,6 +34,9 @@ class ticker_converter:
             return 'ADYEY'
         if ticker == 'CMIIU':
             return 'SLGC'
+        # https://www.globenewswire.com/en/news-release/2022/07/25/2484749/11974/en/Pluristem-Therapeutics-Inc-Changes-its-Name-to-Pluri-Inc-Reflecting-the-Company-s-Strategy-to-Leverage-its-Innovative-3D-Cell-based-Technology-Platform-to-Additional-Industries.html
+        if ticker == 'PSTI':
+            return 'PLUR' # 2022-07-25 change from Pluristem Therapeutics (Nasdaq: PSTI) to Pluri Inc. (Nasdaq: PLUR)
         if (ticker in (
             'CMLF',
             'ACIC', 
@@ -50,7 +53,8 @@ class ticker_converter:
             'RAVN', # CNH Industrial Completes the Acquisition of Raven Industries
             'XONE', # ExOne will continue to operate as a wholly owned subsidiary of Desktop Metal
             'XLNX'  # XLNX acquired by AMD.
-            ,)):
+            'ZY'  # 2022-10-19 Ginkgo Bioworks Completes Acquisition of Zymergen
+            )):
             return None
         
         return ticker
